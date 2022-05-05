f9f8efa0cac303ef70ea1b5312767fc89225868c
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-05 12:39:47 +0000

    index.python

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 
 size = input("What size: ")
 toppings = int(input("Enter amount of toppings: "))
@@ -13,17 +12,20 @@ elif size == "MEDIUM":
 else:
 	size_price = 15
 
-HST = 0.13
+HST = 0.1805
 delivery_cost = 11
-price_delivery = (size_price + (toppings * 0.67) + delivery_cost) * 0.13
-price_no_delivery = (size_price + (toppings * 0.67)) * 0.13
+total = size_price + toppings * 0.50
+price_with_delivery = total + delivery_cost
 
 if (size == "SMALL") or (size == "MEDIUM") or (size == "LARGE"):
 	if (toppings >= 0) and (toppings <= 6):
-		if delivery == "YES":
-			print("The total is", price_delivery)
-		else:
-			print("The total is", price_no_delivery)
+		if (delivery == "YES") or (delivery == "NO"):
+			if delivery == "YES":
+				print("The total is $" + str(price_with_delivery))
+			else:
+				print("The total is $" + str(total))
+		else: 
+			print("Please enter either yes or no for if you need delivery")
 	else:
 		print("Please choose a valid number of toppings. We have 6 diffrent choices.")
 else: 
